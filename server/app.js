5455e2db13ddfaaa3ce4f30b120d6a3f94f7ab07
Tyythedeveloper33 <webstertyler81@yahoo.com>
2024-08-05 22:54:54 -0400

    adding server folder , figuring out how to intergrade sequelize for orm

--- server/app.js
+++ server/app.js
@@ -3,16 +3,16 @@ const path = require('path');
 const app = express();
 const port = 5005;
 
-const { Clothing , Shirts } = require('./models/clothing');
+const { Clothing , Shirts } = require('../models/clothing');
 const products = [];
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 products.push(new Clothing('Nike Air Max', 199.99, 'Shoes', 'https://example.com/air-max.jpg'));
 products.push(new Shirts('Adidas T-Shirt', 29.99, 'Shirts', 'https://example.com/t-shirt.jpg'));
 
 // Import and use routes from the controllers directory
-const routes = require('./controllers');
+const routes = require('../controllers');
 app.use('/', routes);
 
 // Handle 404 errors
